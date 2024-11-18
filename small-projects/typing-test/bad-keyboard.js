b1299cc18cdb8e65b5e2a4a4243446d8bd16c842
Kai Williams <154692413+ChiWilliams@users.noreply.github.com>
2024-11-18 13:28:03 -0500

    in small-projects, added timer to typing-test

--- small-projects/typing-test/bad-keyboard.js
+++ small-projects/typing-test/bad-keyboard.js
@@ -1,27 +1,58 @@
 let KEYBOARD = {}
 const LETTERS = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 const textArea = document.querySelector('textarea');
+let TIMERSTARTED = false;
+let FINISHED = false;
+const GOAL = document.getElementById("goal-string").textContent;
 
 document.addEventListener('DOMContentLoaded', e => {
     KEYBOARD = loadKeyboard();
+    textArea.value = "";
     console.log(KEYBOARD)
-})
+});
 
-textArea.addEventListener('keydown', e => {
+textArea.addEventListener('keydown', async e => {
+    //start the timer
+    if (!TIMERSTARTED) {
+        runTimer();
+        TIMERSTARTED = true;
+    }
+
     const lowerKey = e.key.toLowerCase()
     const isUpper = !(e.key == lowerKey);
-    const isLetter = ( e.key.toUpperCase() != e.key.toLowerCase() );
     if (LETTERS.includes(lowerKey)) {
         e.preventDefault();
-        e.stopPropagation();
+        //e.stopPropagation();
 
+        //transform to new key:
         let newLetter = KEYBOARD[lowerKey];
         if (isUpper) {
             newLetter = KEYBOARD[lowerKey].toUpperCase();
         }
-        textArea.value += newLetter;
+
+        // Update the textarea value manually
+        const cursorPosition = textArea.selectionStart;
+        const textBefore = textArea.value.slice(0, cursorPosition);
+        const textAfter = textArea.value.slice(cursorPosition);
+        textArea.value = textBefore + newLetter + textAfter;
+        // Set the cursor position correctly
+        textArea.selectionStart = textArea.selectionEnd = cursorPosition + 1;
+        console.log("adding letter finished")
     }
-})
+});
+
+//it wouldn't be fun if the user can just paste their input
+//so being evil, we prevent them from doing so ...
+addEventListener("paste", (e) => {
+    //e.preventDefault();
+});
+
+textArea.addEventListener("input", (event) => {
+    if (textArea.value == GOAL) {
+        console.log("YOU WON!!")
+        gameWon();
+    }
+  });
 
 function loadKeyboard() {
     let shuffled = LETTERS
@@ -32,6 +63,27 @@ function loadKeyboard() {
     return keyboardDict;
 }
 
+async function runTimer() {
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    const timer = document.getElementById("timer")
+    let secs = 0;
+    while (!FINISHED) {
+        await delay(1000);
+        secs += 1;
+        timer.innerHTML = `<b>Timer:</b> ${secs}`;
+    }
+    
+}
+
+function gameWon() {
+    // stop the timer
+    FINISHED = true;
+    //make textARea disabled
+    textArea.disabled = true;
+    //show a message:
+    document.getElementById("you-won").style.display = "block";
+
+}
 
 // function shuffle(array) {
 //     let currentIndex = array.length;
